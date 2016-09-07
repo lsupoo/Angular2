32cd4d7e003bca051e8ad038ffbfb763acd4b1fd
Luis Alberto Supo Orihuela <lsupo@belatrix.com>
2016-09-07 16:04:55 -0500

    Terminando modulo 6

--- APM - Start/app/app.component.ts
+++ APM - Start/app/app.component.ts
@@ -1,9 +1,14 @@
-import { Component } from 'angular2/core'
+import { Component } from 'angular2/core';
+import {ProductListComponent} from './product/product-list.component';
 
 @Component(
     {
         selector : 'pm-app',
-        template : '<div><h1>{{pageTitle}}</h1><div>My first Component</div></div>'
+        template : `<div>
+                    <h1>{{pageTitle}}</h1>
+                    <pm-products></pm-products>
+                    </div>`,
+        directives: [ProductListComponent]
     }
 )
 
